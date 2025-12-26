75249f945b5a450ff9c73c33a989a2f740b7cfba
kz-17 <s111310533@student.nqu.edu.tw>
2025-12-26 18:31:38 +0800

    Update Mult.asm

--- mid/4/Mult.asm
+++ mid/4/Mult.asm
@@ -8,3 +8,31 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+// 初始化 R2 為 0
+    @R2
+    M=0
+
+(LOOP)
+    // 檢查 R1 是否大於 0，如果 R1 <= 0 則跳到結束
+    @R1
+    D=M
+    @END
+    D;JLE    // If R1 <= 0 goto END
+
+    // 執行加法：R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // 計數器減 1：R1 = R1 - 1
+    @R1
+    M=M-1
+
+    // 回到迴圈起點
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP    // 無窮迴圈，代表程式結束
